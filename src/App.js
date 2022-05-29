11a8d7095395f86a8e5555f2c500dd8070b90440
Sayalikanade <sayalikanade97@gmail.com>
2022-05-29 15:10:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [ joke,setjoke] = useState("loading")
+  const [fname,setFN] = useState("john")
+  const [sname,setSN] = useState("doe")
+
+  const newjoke = (first,second)=>{
+    fetch('http://api.icndb.com/jokes/random?firstName=john&lastName=doe')
+    .then(res=>res.json())
+    .then(res2=>{
+      console.log(res2)
+       setjoke(res2.value.joke)
+       })
+  }
+  useEffect(()=>{
+    newjoke(fname,sname)
+},[fname,sname])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Coders Never Quit</h3>
+      <input type="text" value={fname} onChange={(e)=>setFN(e.target.value)} />
+      <input type="text" value={sname} onChange={(e)=>setSN(e.target.value)} />
+      <button onClick={()=>newjoke()}>Get Another Joke</button>
+      <h4>{joke}</h4>
     </div>
   );
 }
